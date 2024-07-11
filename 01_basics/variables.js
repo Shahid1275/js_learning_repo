08343f78b55f2e01272455a846a36ee759d9a976
SHAHID AMEEN <156401527+Shahid1275@users.noreply.github.com>
2024-07-11 13:22:52 +0000

    some code

---
+++ 01_basics/variables.js
@@ -0,0 +1,5 @@
+// let and const are most probably used variables in js for store everything like
+let firstName = "Shahid";
+let lastName = "Ameen Sajid";
+let fullName =(firstName +" " + lastName);
+console.table[(firstName,lastName,fullName)];
---
+++ datatypes.js
@@ -0,0 +1,5 @@
+let name = "shahid";
+let num = 99;
+let boolean = true;
+let status = null;
+console.log(typeof "name");
